55f1ddb4195298304e9003db6ba2a1332a5d2835
raph <raph@hpar.fr>
2015-05-27 15:14:18 +0200

    +list impl

--- www/list/list.ctrl.js
+++ www/list/list.ctrl.js
@@ -1,6 +1,11 @@
 'use strict';
 
 
-angular.module('starter').controller('ListCtrl', ['$scope',function ($scope) {
+angular.module('starter').controller('ListCtrl', ['$scope', 'jsonRpc', function ($scope, jsonRpc) {
 	console.log('ctrl !');
+	$scope.list = null;
+	jsonRpc.call('mrp.workcenter', 'prodoo_get_workcenter', []).then(function (data) {
+		console.log('data !!', data);
+		$scope.list = data;
+	});
 }]);
