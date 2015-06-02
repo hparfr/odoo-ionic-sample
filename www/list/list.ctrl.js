7e6f2abe9d4d414679310f13a9255b6076633e85
Sébastien BEAU <sebastien.beau@akretion.com>
2015-06-02 10:52:22 +0200

    [REF] refactor

--- www/list/list.ctrl.js
+++ www/list/list.ctrl.js
@@ -3,9 +3,4 @@
 
 angular.module('starter').controller('ListCtrl', ['$scope', 'jsonRpc', function ($scope, jsonRpc) {
 	console.log('ctrl !');
-	$scope.list = null;
-	jsonRpc.call('mrp.workcenter', 'prodoo_get_workcenter', []).then(function (data) {
-		console.log('data !!', data);
-		$scope.list = data;
-	});
 }]);
