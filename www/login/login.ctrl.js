dc7a2d5d2d9b830580d83b5076b3816f1e6d7b38
raph <raph@hpar.fr>
2015-06-11 14:19:40 +0200

    + feature: connect to another server

--- www/login/login.ctrl.js
+++ www/login/login.ctrl.js
@@ -5,10 +5,12 @@ angular.module('starter').controller('LoginCtrl', ['$scope', 'jsonRpc', '$state'
 
 	$scope.login = {
 		'db': 'db',
-		'username':'admin'
+		'username':'admin',
+		'server': ''
 	};
 	$scope.submit = function () {
 		console.log('send', $scope.login);
+		jsonRpc.odoo_server = $scope.login.server;
 		jsonRpc.login($scope.login.db, $scope.login.username, $scope.login.password).then(function (a) {
 			$state.go('list');
 		}, function(e) {
